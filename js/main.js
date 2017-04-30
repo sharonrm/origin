5bc14254bebecb9f657a5bf5ef9f0f04eb3d1873
Sharon Moskal <sharonmoskal@icloud.com>
2017-04-30 15:35:02 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
- var cardOne = "King";
- var cardTwo = "King";
- var cardThree = "Queen";
- var cardFour = "Queen";
- console.log ("User flipped " + cardOne);
- console.log ("User flipped " + cardThree);
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay =[];
+var cardOne;
+cardOne = cards[0];
+cardsInPlay.push (cardOne);
+console.log ("User flipped " + cardOne);
+var cardTwo;
+cardTwo = cards[2];
+cardsInPlay.push (cardTwo);
+console.log ("User flipped " + cardTwo);
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay [0] === cardsInPlay [1]) 
+	{
+       alert ("You found a match");
+	} else {
+		alert ("Sorry, try again");
+	}
+}
